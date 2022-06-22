78894d7a042fdbd2056d4412784f609b48cf2a73
eduard1dev <car.edu.eng@gmail.com>
2022-06-22 00:41:12 -0300

    check env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { parseCookies } from 'nookies';
 
 export const getApiClient = (ctx?: any) => {
   const { 'nextauth.token': token } = parseCookies(ctx);
-
+console.log(process.env.REACT_ENV)
 const api = axios.create({
   baseURL: process.env.REACT_ENV == 'production' ? 'https://meache-api.ed1labs.xyz/' : 'http://localhost:5000'
 });
